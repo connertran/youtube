70bbe3d472f259682e338bad957fbcec847c4de5
Conner <conner.tran@bsa.edu.vn>
2025-08-06 12:36:29 +0200

    single, double, triple

--- script.js
+++ script.js
@@ -15,18 +15,38 @@ var longestPalindrome = function (s) {
     let frontletter = s[i - position];
     let backletter = s[i + position];
     let backletterDouble = s[i + position + 1];
+    let frontletterDouble = s[i - position - 1];
 
     if (s[i] === backletter) {
-      current = s[i] + backletter;
-      current.length > best.length ? (best = current) : (best = best);
-      while (frontletter && backletterDouble) {
-        console.log(`double: ${frontletter} & ${backletterDouble}`);
-        if (frontletter === backletter) {
-          current = frontletter + current + backletterDouble;
-          current.length > best.length ? (best = current) : (best = best);
-          position++;
-        } else {
-          break;
+      if (s[i] === frontletter) {
+        current = frontletter + s[i] + backletter;
+        current.length > best.length ? (best = current) : (best = best);
+        while (frontletterDouble && backletterDouble) {
+          console.log(`triple: ${frontletterDouble} & ${backletterDouble}`);
+          if (frontletterDouble === backletterDouble) {
+            current = frontletterDouble + current + backletterDouble;
+            current.length > best.length ? (best = current) : (best = best);
+            position++;
+            backletterDouble = s[i + position + 1];
+            frontletterDouble = s[i - position - 1];
+          } else {
+            break;
+          }
+        }
+      } else {
+        current = s[i] + backletter;
+        current.length > best.length ? (best = current) : (best = best);
+        while (frontletter && backletterDouble) {
+          console.log(`double: ${frontletter} & ${backletterDouble}`);
+          if (frontletter === backletterDouble) {
+            current = frontletter + current + backletterDouble;
+            current.length > best.length ? (best = current) : (best = best);
+            position++;
+            frontletter = s[i - position];
+            backletterDouble = s[i + position + 1];
+          } else {
+            break;
+          }
         }
       }
     } else {
@@ -37,6 +57,8 @@ var longestPalindrome = function (s) {
           current = frontletter + current + backletter;
           current.length > best.length ? (best = current) : (best = best);
           position++;
+          frontletter = s[i - position];
+          backletter = s[i + position];
         } else {
           break;
         }
