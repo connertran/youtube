167326b13ed3e95e957062ac65798aebe8726d9c
Conner <conner.tran@bsa.edu.vn>
2025-08-07 11:28:00 +0200

    finished the Roman-to-integer question

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000,
   };
+  let result = 0;
+  let lastNum = romanObj[s[s.length - 1]];
+  for (let i = s.length - 1; i >= 0; i--) {
+    let currentNum = romanObj[s[i]];
+    if (currentNum >= lastNum) {
+      result += currentNum;
+    } else {
+      result -= currentNum;
+    }
+
+    lastNum = romanObj[s[i]];
+  }
+  return result;
 };
+
+console.log(romanToInt("MCMXCIV"));
