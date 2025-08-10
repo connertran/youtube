3921234b59ad935666adced97eefad2fb1e263e8
Conner <conner.tran@bsa.edu.vn>
2025-08-10 15:33:24 +0200

    finished Product of Array Except Self

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 /**
- * @param {string} s
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[]}
  */
-var lengthOfLastWord = function (s) {
-  let length = s.length;
-  let result = 0;
-  let startCounting = false;
+var productExceptSelf = function (nums) {
+  const leftRight = {};
+  const rightLeft = {};
+  let leftValue = 1;
+  let rightValue = 1;
+  for (let i = 0; i < nums.length; i++) {
+    leftValue = leftValue * nums[i];
+    leftRight[i] = leftValue;
 
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] !== " ") {
-      startCounting = true;
-      result++;
-    } else if (s[i] === " " && startCounting) {
-      break;
-    }
-    length--;
+    rightValue = rightValue * nums[nums.length - 1 - i];
+    rightLeft[nums.length - 1 - i] = rightValue;
   }
-  return result;
+  console.log(leftRight);
+  console.log(rightLeft);
+  for (let i = 0; i < nums.length; i++) {
+    let leftProduct = 1;
+    let rightProduct = 1;
+    if (leftRight[i - 1] !== undefined) leftProduct = leftRight[i - 1];
+    if (rightLeft[i + 1] !== undefined) rightProduct = rightLeft[i + 1];
+    nums[i] = leftProduct * rightProduct;
+  }
+
+  return nums;
 };
 
-console.log(lengthOfLastWord("a"));
+console.log(productExceptSelf([1, 2, 3, 4]));
