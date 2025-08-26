143290a58a11820f56bed0b378c9a025fdfa2862
Conner <conner.tran@bsa.edu.vn>
2025-08-26 16:12:26 +0200

    finished 112 path sum

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-var longestConsecutive = function (nums) {
-  if (nums.length === 0) {
-    return 0;
-  }
-  const existenceSet = new Set(nums);
-  let best = 1;
-  for (let num of existenceSet) {
-    if (!existenceSet.has(num - 1)) {
-      let count = 1;
-      while (existenceSet.has(num + 1)) {
-        count++;
-        num++;
-      }
-      best < count ? (best = count) : (count = count);
-    }
-  }
-  return best;
-};
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (root === null) return false;
 
-console.log(longestConsecutive([1, 0, 1, 2]));
+  let sum = 0;
+  function recursion(node) {
+    if (node === null) return false;
+
+    sum += node.val;
+    if (node.left === null && node.right === null) {
+      if (sum === targetSum) {
+        sum -= node.val;
+        return true;
+      }
+      sum -= node.val;
+      return false;
+    }
+
+    const hasLeft = recursion(node.left);
+    const hasRight = recursion(node.right);
+
+    sum -= node.val;
+
+    return hasLeft || hasRight;
+  }
+
+  return recursion(root) === true;
+};
