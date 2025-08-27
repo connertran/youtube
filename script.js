bc205b7d10f06fbcb8c594ba403bc9505e316eb1
Conner <conner.tran@bsa.edu.vn>
2025-08-27 12:16:19 +0200

    finished 61 Rotate list

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @return {boolean}
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
  */
-var hasPathSum = function (root, targetSum) {
-  if (root === null) return false;
-
-  let sum = 0;
-  function recursion(node) {
-    if (node === null) return false;
-
-    sum += node.val;
-    if (node.left === null && node.right === null) {
-      if (sum === targetSum) {
-        sum -= node.val;
-        return true;
-      }
-      sum -= node.val;
-      return false;
-    }
-
-    const hasLeft = recursion(node.left);
-    const hasRight = recursion(node.right);
-
-    sum -= node.val;
-
-    return hasLeft || hasRight;
+var rotateRight = function (head, k) {
+  if (k === 0) {
+    return head;
   }
-
-  return recursion(root) === true;
+  if (head === null) {
+    return null;
+  }
+  if (head.next === null) {
+    return head;
+  }
+  const linkedListStack = [];
+  let currentNode = head;
+  while (currentNode) {
+    linkedListStack.push(currentNode);
+    if (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    } else {
+      break;
+    }
+  }
+  let result = head;
+  let rotate = k % linkedListStack.length;
+  while (rotate > 0) {
+    const lastNode = linkedListStack.pop();
+    linkedListStack[linkedListStack.length - 1].next = null;
+    lastNode.next = result;
+    result = lastNode;
+    linkedListStack.unshift(lastNode);
+    rotate--;
+  }
+  return result;
 };
