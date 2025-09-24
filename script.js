8d30b432810e64a90924f443806a93905e59b369
Conner <conner.tran@bsa.edu.vn>
2025-09-24 17:02:48 +0200

    finished 125. Valid Palindrome

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-var rotate = function (nums, k) {
-  const n = nums.length;
-  if (n === 0) return;
-  k %= n;
-  if (k === 0) return;
-
-  let moved = 0;
-  for (let start = 0; moved < n; start++) {
-    let current = start;
-    let prev = nums[start];
-
-    do {
-      const next = (current + k) % n;
-      [nums[next], prev] = [prev, nums[next]];
-      current = next;
-      moved++;
-    } while (current !== start);
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const alphabetSet = new Set([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ]);
+  const string = s.split("");
+  const arrayAlphabet = string.filter((ch) => {
+    if (alphabetSet.has(ch.toLocaleLowerCase())) {
+      return ch;
+    }
+  });
+  let start = 0;
+  let end = arrayAlphabet.length - 1;
+  while (start <= end) {
+    if (start === end) {
+      return true;
+    }
+    if (
+      arrayAlphabet[start].toLocaleLowerCase() !==
+      arrayAlphabet[end].toLocaleLowerCase()
+    ) {
+      return false;
+    }
+    start++;
+    end--;
   }
+  return true;
 };
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
